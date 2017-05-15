9a110b0b56bdffba2129be10b52484418c45d9ad
binhui.xu <binhui.xu@travelzen.com>
2017-05-15 17:43:34 +0800

    project init

---
+++ src/main/java/it/ldlife/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package it.ldlife.dao;
+
+import it.ldlife.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/it/ldlife/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package it.ldlife.dao;
+
+import it.ldlife.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
